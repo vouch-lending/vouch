b6257bd1123b7be0e571847cc1558c838dde7b03
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2023-07-21 16:07:16 +0100

    feat: card added - dummy data

---
+++ app/components/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Dashboard: FC = () => {
+  return (
+    <div className="h-screen-minus flex items-center justify-center">
+        
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ app/pages/loans.tsx
@@ -0,0 +1,8 @@
+import Loans from '@/components/loans'
+import { FC } from 'react'
+
+const LoansPage: FC = () => {
+  return <Loans />
+}
+
+export default LoansPage
