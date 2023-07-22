f8a234e8e110a5c53a575634118efdaf793d0362
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2023-07-23 00:37:20 +0100

    feat: seo

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,4 +1,4 @@
 import abi from './abi.json'
 
 export const VOUCH_ABI = abi
-export const VOUCH_ADDRESS = '0x9155497eae31d432c0b13dbcc0615a37f55a2c87'
+export const VOUCH_ADDRESS = '0xA29AAe7F645821d8A9939843824aBE4aBCf3b198'
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,11 +1,32 @@
 import Layout from '@/components/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Vouch</title>
+        <meta name="title" content="Vouch" />
+        <meta
+          name="description"
+          content="The future of lending is here with undercollateralised loans"
+        />
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=yes"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="author" content="Ahmed & Efeosa w/ <3" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
